57da5fe20c8595725597f545483d06b9dcaba624
aleksandr2639 <alexanderkriulin@yandex.ru>
2024-03-09 15:01:42 +0300

    add: file project

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import './App.css'
+import moment from 'moment';
+
+const hoc = (Component) => (props) => {
+      return (
+          <>
+              <Component {...props} date={moment(props.date).fromNow()}/>
+          </>
+      )
+}
+
+function DateTime(props) {
+    return (
+        <p className="date">{props.date}</p>
+    )
+}
+
+const DateTimeConvert = hoc(DateTime)
+
+function Video(props) {
+    return (
+        <div className="video">
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <DateTimeConvert date={props.date} />
+        </div>
+    )
+}
+
+function VideoList(props) {
+    return props.list.map(item => <Video url={item.url} date={item.date} />);
+}
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-07-31 13:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-03-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-02-03 23:16:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2024-03-07 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2024-03-09 15:24:00'
+        },
+    ]);
+
+    return (
+        <VideoList list={list} />
+    );
+}
